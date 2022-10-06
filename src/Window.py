b37fa6f7134376b2d700916826bf5baee1640590
djmaster458 <djosbo04@louisville.edu>
2022-10-06 16:25:10 -0400

    Cleanup

--- src/Window.py
+++ src/Window.py
@@ -5,7 +5,7 @@ APPNAME='Digi-Vis'
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
+        self.label = tk.Label(self, text='Hello World', padx=5, pady=5)
         self.label.pack()
         self.geometry('800x600')
         self.title(APPNAME)
