860d5d88404cf430fda7f9b546a1d30940045c61
djmaster458 <djosbo04@louisville.edu>
2022-10-22 10:53:55 -0400

    In Progress

--- src/Frame/DataBuilderFrame.py
+++ src/Frame/DataBuilderFrame.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from src.Frame.SelectUserAndDateFrame import SelectUserAndDateFrame
 from src.Frame.ColumnSelectorFrame import ColumnSelectorFrame
 
@@ -41,56 +39,3 @@ class DataBuilderFrame(tk.Frame):
         
         # Create Visual Frame
         # Add To Notebook, Maybe Set That Tab as active
-
-#     def chooseCols(self):
-#         self.pathToCSV = os.path.join(self.fileSelectorFrame.GetPathToFiles(), 'summary.csv')
-#         self.fileSelectorFrame.destroy()
-#         self.button.destroy()
-#         self.colChooseGroup = ChooseColumns(self, self.pathToCSV)
-#         self.colChooseGroup.pack(anchor=tk.W)
-
-#         self.button = tk.Button(self, text="Save", command=self.display)
-#         self.button.pack(ipadx=1, ipady=1)
-
-#     def display(self):
-#         self.CSVData = self.colChooseGroup.getCSVData()
-#         self.chosenCols = self.colChooseGroup.getChoseCols()
-#         self.colChooseGroup.destroy()
-#         self.button.destroy()
-#         self.dataViewGroup = DisplayData(self, self.CSVData, self.chosenCols)
-#         self.dataViewGroup.pack(anchor=tk.W)
-
-
-# class DisplayData(tk.Frame):
-#     def __init__(self, root, CSVData, columns):
-#         """Initialize a Data Displayer for displaying the data from a specific CSV"""
-#         super().__init__(root, highlightbackground="blue", highlightthickness=2)
-#         self.root = root
-#         self.df = CSVData
-#         self.columns = columns
-#         self.setupFrameWidget()
-
-
-#     def readData(self):
-#         """Read the data from the target CSV to df"""
-#         columns = ["Datetime (UTC)", "Acc magnitude avg", "Eda avg", "Temp avg"]
-#         self.df = pd.read_csv(self.pathToCSV, usecols=columns)
-
-
-#     def setupFrameWidget(self):
-#         """Create matplotlib graph, plot points, and display to window"""
-#         if "Datetime (UTC)" in self.columns:
-#             self.df["Datetime (UTC)"] = pd.to_datetime(self.df["Datetime (UTC)"])
-#             self.df["Datetime (UTC)"] = self.df["Datetime (UTC)"].dt.strftime('%H:%M:%S')
-#         figure = plt.Figure(figsize=(15,10), dpi=100)
-#         ax = figure.add_subplot(111)
-#         chart_type = FigureCanvasTkAgg(figure, self)
-#         chart_type.get_tk_widget().pack()
-#         if "Datetime (UTC)" in self.columns:
-#             self.df[self.columns].set_index('Datetime (UTC)').plot(rot=0, kind='line', legend=True, ax=ax)
-#             ax.set_xlabel('Time (HH:MM:SS)')
-#         else:
-#             self.df[self.columns].plot(rot=0, kind='line', legend=True, ax=ax)
-#             ax.set_xlabel('Row Number')
-#         ax.set_title('Data Line Chart')
-#         ax.set_ylabel('Value')
